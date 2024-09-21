93e04850122d669e14e1318624d99b274b2ea73a
hkalbasi <hamidrezakalbasi@protonmail.com>
2024-09-21 14:37:35 +0330

    Add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      fhs = pkgs.buildFHSUserEnv {
+        name = "fhs-shell";
+        targetPkgs = pkgs: [ pkgs.rye ];
+      };
+    in
+    {
+      devShells.${system}.default = fhs.env;
+    };
+}
